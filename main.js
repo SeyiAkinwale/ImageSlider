f9c3aeb6c90f5baa899907b485737694120f8674
SeyiAkinwale <72584469+SeyiAkinwale@users.noreply.github.com>
2023-07-26 17:51:03 +0000

    js script

--- main.js
+++ main.js
@@ -1,94 +1,53 @@
-// Variables
-let nextBtn = document.querySelector(".next-btn");
-let prevBtn = document.querySelector(".prev-btn");
-let tile = document.querySelectorAll(".box");
-let listLength = tile.length - 1;
 
-/* Set interface at page load */
-prevBtn.setAttribute("disabled", "");
+    $(document).ready(function () {
+        $.getJSON("data-with-image.json", function (data) {
 
+            var arrItems = [];      // The array to store JSON items.
+            $.each(data, function (index, value) {
+                arrItems.push(value);       // Push values in the array.
+            });
 
-// Counter from MDN Closures article; https://developer.mozilla.org/en-US/docs/Web/JavaScript/Closures
-let count = (function() {
-  let privateCounter = 0;
-  function changeBy(val) {
-    privateCounter += val;
-  }
+            // Extract values for the table header.
+            var col = [];
+            for (var i = 0; i < arrItems.length; i++) {
+                for (var key in arrItems[i]) {
+                    if (col.indexOf(key) === -1) {
+                        col.push(key);
+                    }
+                }
+            }
 
-  return {
-    countUp: function() {
-      changeBy(1);
-    },
-    countDown: function() {
-      changeBy(-1);
-    },
-    value: function() {
-      return privateCounter;
-    }
-  };
-})();
+            // Create a <table> element dynamically.
+            // Ref: https://www.encodedna.com/javascript/populate-json-data-to-html-table-using-javascript.htm 
+            var table = document.createElement("table");
 
+            var tr = table.insertRow(-1);                   // Table row.
 
-function selectNext() {
-  let currentIndex = count.value();
+            for (var i = 0; i < col.length; i++) {
+                var th = document.createElement("th");      // Table header.
+                th.innerHTML = col[i];
+                tr.appendChild(th);
+            }
 
-  currentIndex = currentIndex + 1;
+            // Add JSON data to the table as rows.
+            for (var i = 0; i < arrItems.length; i++) {
+                tr = table.insertRow(-1);
 
-  //console.log(tile.length);
+                for (var j = 0; j < col.length; j++) {
+                    var tabCell = tr.insertCell(-1);
+                    if (j === 2) {      // The last JSON column has image urls.
 
-  /* Increment count value */
-  count.countUp();
+                        // Create an <img> element to show the images.
+                        var img = document.createElement('img');        
+                        img.src = arrItems[i].Image;   // The image source from JSON array.
+                        tabCell.appendChild(img);
+                    }
+                    else
+                        tabCell.innerHTML = arrItems[i][col[j]];
+                }
+            }
 
-  //console.log(currentIndex);
-  //console.log(listLength);
-
-  /* Add highlighting to the next tile */
-  if (tile[currentIndex] !== undefined) {
-    tile[currentIndex].classList.add("is-selected");
-  }
-
-  /* Remove from previous tile */
-  if (tile[currentIndex - 1] !== undefined) {
-    tile[currentIndex - 1].classList.remove("is-selected");
-  }
-
-  /* Set next image button to disabled if we are on the last tile;
-   * set previous button to enabled */
-  if (currentIndex === listLength) {
-    nextBtn.setAttribute("disabled", "");
-  } else {
-    prevBtn.removeAttribute("disabled");
-  }
-}
-
-
-function selectPrev() {
-  let currentIndex = count.value();
-
-  /* Decrement count value */
-  count.countDown();
-
-  /* Add highlighting to previous tile */
-  if (currentIndex > 0 && tile[currentIndex - 1] !== undefined) {
-    tile[currentIndex - 1].classList.add("is-selected");
-  }
-
-  /* Remove from current tile */
-  if (tile[currentIndex] !== undefined) {
-    tile[currentIndex].classList.remove("is-selected");
-  }
-
-  /* Set previous image button to disabled if we are on the first image tile;
-   * set next button to enabled
-   */
-  if (currentIndex - 1 <= 0) {
-    prevBtn.setAttribute("disabled", "");
-  } else {
-    nextBtn.removeAttribute("disabled");
-  }
-}
-
-
-// Event Listeners
-nextBtn.addEventListener("click", selectNext);
-prevBtn.addEventListener("click", selectPrev);
+            // Finally, add the newly created <table> with data to a container.
+            var divContainer = document.getElementById("showData");
+            divContainer.innerHTML = "";
+            divContainer.appendChild(table);
